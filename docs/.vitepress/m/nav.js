3372713517908bd25c524802bad64e8922e8046f
wxhzh <mygod@qq.com>
2025-09-09 11:34:16 +0800

    装机必备软件和目录结构调整

--- docs/.vitepress/m/nav.js
+++ docs/.vitepress/m/nav.js
@@ -14,12 +14,9 @@ export const nav_config = [
   { text: '下载', link: '/download_zone/下载专区' },
   { text: '法宝', link: '/download_zone/法宝专区' },
   { text: '变异骚神库', link: '/download_zone/变异版DP' },
-  { text: '程序员手册', link: 'http://8.152.211.132:9667/index.html' },
   {
-    text: '💋实用工具',
-    items: [
-      { text: '0.远程操作浏览器', link: '/study/remote_dp/remote_dp.md' },
-      { text: '0.🌏️全能导航', link: '/public/hao123' },
+    text: '网站', items: [
+      { text: '0. 全能导航', link: '/public/hao123' },
       { text: '1.DP官网', link: 'http://drissionpage.cn/' },
       { text: '2.🐛虫盒', link: 'https://spiderbox.cn/' },
       { text: '3.vitpress教学', link: 'https://docs.bugdesigner.cn/docs/Tutorial/vitepress.html' },
@@ -35,35 +32,41 @@ export const nav_config = [
       { text: '13.爬虫靶场', link: 'https://scrape.center/' },
       { text: '14.指纹检测', link: 'https://bot.sannysoft.com/' },
       { text: '15.JSON4U 在线编辑器', link: 'https://json4u.cn/editor' },
-      { text: '16.DrissionPage工具库', link: 'https://wxhzhwxhzh.github.io/dptool' },
-      { text: '17.腾讯文档插件手册', link: 'https://docs.qq.com/aio/p/sc4xdbyayxdf36f?p=CDNmOdRjNMOM5hGBSjJQ2X' },
-      { text: '18.JSON可视化', link: 'https://jsoncrack.com/editor' },
-      { text: '19.WebSocket在线测试', link: 'http://www.websocket-test.com/' },
-      { text: '20.x-cmd', link: 'https://cn.x-cmd.com/' },
-      { text: '21.DrissionPage代码启动助手', link: 'https://wxhzhwxhzh.github.io/dpcode/' },
     ]
   },
   {
-    text: '其他',
-    items: [      
-      { text: 'newsNow', link: 'https://newsnow.busiyi.world/' },
-      { text: 'Linux换源', link: 'https://linuxmirrors.cn/other/' },
-      { text: '爱壹帆排行榜', link: 'https://www.iyf.tv/rank/all' },
-      { text: '绿色谷歌浏览器', link: '/study/green_chrome.md' },
-      { text: 'RunWeb', link: 'https://uiwjs.github.io/react-run-web/' },
-      { text: 'github rank', link: 'https://wangchujiang.com/github-rank/trending.html' },
-      { text: 'chrome插件官方文档', link: 'https://developer.chrome.com/docs/extensions?hl=zh-cn' },
-      { text: '换源教程', link: '/study/huanyuan.md' },
-      { text: 'docker换源教程', link: '/study/docker_huanyuan.md' },
-      { text: ' LibreTV', link: 'http://8.152.211.132:8989/' },
-      { text: ' MoonTV', link: 'http://8.152.211.132:8990/login' },
-      { text: ' Docker指南', link: '/study/docker.md' },
-      { text: ' Xget', link: '/study/xget.md' },
-      { text: ' vim', link: '/study/vim.md' },
-      { text: ' nano', link: '/study/nano.md' },
-      { text: ' pycharm', link: '/study/pycharm.md' },
-      { text: ' vscode', link: '/study/vscode.md' },
-      { text: ' McpJson', link: '/study/MCPJSON.md' },
+    text: "💋实用工具",
+    items: [
+      { text: "1. 远程操作浏览器", link: "/study/remote_dp/remote_dp.md" },
+      { text: "2. DrissionPage工具库", link: "https://wxhzhwxhzh.github.io/dptool" },
+      { text: "3. 腾讯文档插件手册", link: "https://docs.qq.com/aio/p/sc4xdbyayxdf36f?p=CDNmOdRjNMOM5hGBSjJQ2X" },
+      { text: "4. JSON可视化", link: "https://jsoncrack.com/editor" },
+      { text: "5. WebSocket在线测试", link: "http://www.websocket-test.com/" },
+      { text: "6. 👑装机必备软件", link: "/study/software.md" },
+      { text: "7. DrissionPage代码启动助手", link: "https://wxhzhwxhzh.github.io/dpcode/" }
+    ]
+  },
+  {
+    text: "其他",
+    items: [
+      { text: "1. newsNow", link: "https://newsnow.busiyi.world/" },
+      { text: "2. Linux换源", link: "https://linuxmirrors.cn/other/" },
+      { text: "3. 爱壹帆排行榜", link: "https://www.iyf.tv/rank/all" },
+      { text: "4. 绿色谷歌浏览器", link: "/study/green_chrome.md" },
+      { text: "5. RunWeb", link: "https://uiwjs.github.io/react-run-web/" },
+      { text: "6. github rank", link: "https://wangchujiang.com/github-rank/trending.html" },
+      { text: "7. chrome插件官方文档", link: "https://developer.chrome.com/docs/extensions?hl=zh-cn" },
+      { text: "8. 换源教程", link: "/study/huanyuan.md" },
+      { text: "9. docker换源教程", link: "/study/docker_huanyuan.md" },
+      { text: "10. LibreTV", link: "http://8.152.211.132:8989/" },
+      { text: "11. MoonTV", link: "http://8.152.211.132:8990/login" },
+      { text: "12. Docker指南", link: "/study/docker.md" },
+      { text: "13. Xget", link: "/study/xget.md" },
+      { text: "14. vim", link: "/study/vim.md" },
+      { text: "15. nano", link: "/study/nano.md" },
+      { text: "16. pycharm", link: "/study/pycharm.md" },
+      { text: "17. vscode", link: "/study/vscode.md" },
+      { text: "18. McpJson", link: "/study/MCPJSON.md" }
     ]
   }
 ];
---
+++ docs/study/software.md
@@ -0,0 +1,61 @@
+# 常用软件整理（表格版）
+
+## 1. 系统安全维护
+
+| 软件 | 类型 | 说明 | 下载 |
+|------|------|------|------|
+| 7-Zip | 安装 | 解压缩工具 | [下载](https://www.7-zip.org/a/7z2501-x64.exe) |
+| 卡巴斯基 | 安装 | 杀毒软件，参考贴吧置顶帖 | [百度贴吧-病毒吧](https://tieba.baidu.com/f?kw=%E7%97%85%E6%AF%92) |
+| Geek Uninstaller | 便携 | 软件卸载工具 | [下载](https://geekuninstaller.com/geek.zip) |
+| Sandboxie | 安装 | 沙箱（轻量虚拟机） | [下载](https://github.com/sandboxie-plus/Sandboxie/releases/download/v1.16.2/Sandboxie-Classic-x64-v5.71.2.exe) |
+| SpaceSniffer | 便携 | 磁盘分析工具 ⚠️删除前需确认 | [下载](https://download.dappcdn.com/expiretime=1757045897;badurl=aHR0cHM6Ly9kYXBwY2RuLmNvbS9wcm90ZWN0ZWQv/61bb08c4546a80a791960e44a9cdcc0be03a975dfe249d1b4d3805a928764362/spacesniffer/spacesniffer_2_0_3_12_x64.zip) |
+
+---
+
+## 2. 办公软件
+
+| 软件 | 类型 | 说明 | 下载 |
+|------|------|------|------|
+| WPS Office 2023 专业版 | 安装 | 办公套件 | 官网 |
+| 思绪思维导图 | 便携 | 思维导图工具 | [下载](https://github.com/wanglin2/mind-map/releases/download/0.15.0/0.15.0.exe) |
+| Obsidian | 安装 | 笔记软件，B站有教程 | [下载](https://github.com/obsidianmd/obsidian-releases/releases/download/v1.9.12/Obsidian-1.9.12.exe) |
+| 百度网盘 | 安装 | 网盘工具 | [下载](https://pan.baidu.com/download) |
+| 夸克网盘 | 网页端 | 资源分享较多 | [官网入口](https://pan.quark.cn/) |
+
+---
+
+## 3. 游戏
+
+| 软件 | 类型 | 说明 | 下载 |
+|------|------|------|------|
+| Steam | 安装 | 游戏商城（大型平台） | [下载](https://cdn.akamai.steamstatic.com/client/installer/SteamSetup.exe) |
+| Epic Games | 安装 | 游戏商城（每周送游戏） | [下载](https://launcher-public-service-prod06.ol.epicgames.com/launcher/api/installer/download/EpicGamesLauncherInstaller.msi) |
+| 游戏加速器 | - | 外服必备，加速工具 | B站搜索 |
+
+---
+
+## 4. 影音软件
+
+| 软件 | 类型 | 说明 | 下载 |
+|------|------|------|------|
+| Honeyview | 便携 | 图片查看工具 | [下载](https://www.bandisoft.com/honeyview/dl.php?portable) |
+| foobar2000 | 安装/便携 | 音频播放（可定制） | [下载](https://www.foobar2000.org/downloads/foobar2000-x64_v2.25.exe) |
+| VLC | 安装 | 视频播放（全能播放器） | [下载](https://get.videolan.org/vlc/3.0.21/win64/vlc-3.0.21-win64.exe) |
+| ShareX | 便携 | 截图/录屏/取色等综合工具 | [下载](https://github.com/ShareX/ShareX/releases/download/v18.0.1/ShareX-18.0.1-portable.zip) |
+| OBS Studio | 便携 | 录屏 & 串流直播 | [下载](https://cdn-fastly.obsproject.com/downloads/OBS-Studio-31.1.2-Windows-x64.zip) |
+| 剪映 v6.01 | 便携 | 视频剪辑软件（旧版） | [百度网盘](https://pan.baidu.com/s/1u6CeaoPb675oprsR_2-npg?pwd=twhi) 提取码：twhi |
+
+---
+
+## 5. 小工具
+
+| 软件 | 类型 | 说明 | 下载 |
+|------|------|------|------|
+| Clash Verge | 安装 | 网络代理工具 | [下载](https://github.com/clash-verge-rev/clash-verge-rev/releases/download/v2.4.1/Clash.Verge_2.4.1_x64-setup.exe) |
+| qBittorrent | 安装 | 种子/磁力下载器 | [下载](https://sourceforge.net/projects/qbittorrent/files/latest/download) |
+| Everything | 便携 | 文件搜索工具 | [下载](https://www.voidtools.com/Everything-1.4.1.1028.x64.zip) |
+| Mem Reduct | 安装/便携 | 内存清理，加速工具 | [下载](https://memreduct.org/files/memreduct-3.4-setup.exe) |
+| Music Tag | 便携 | 音乐标签（歌词/封面） | [主页](https://www.cnblogs.com/vinlxc) / 蓝奏云（解压密码：www.coolapk.com） |
+| Click Show | 便携 | 鼠标指针高光 | [下载](https://github.com/cuiliang/ClickShow/releases/download/1.4.1/ClickShow1_4_1.zip) |
+| BiliDownloader | 便携 | B站视频下载工具 | [下载](https://github.com/Harlan-H/BiliDownloader/releases/download/1.0.8/BiliDownloader.zip) |
+| Context Menu Manager | 便携 | 右键菜单管理工具 | [下载](https://github.com/BluePointLilac/ContextMenuManager/releases/download/3.3.3.1/ContextMenuManager.zip) |

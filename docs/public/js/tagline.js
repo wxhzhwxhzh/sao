ccd23008b01294e6fd2c631290178cb4f1dd6fd4
wxhzh <mygod@qq.com>
2025-11-14 13:12:54 +0800

    docs: 优化 tagline.js 变量命名并添加调试日志

--- docs/public/js/tagline.js
+++ docs/public/js/tagline.js
@@ -1,7 +1,8 @@
 fetch('https://v1.hitokoto.cn/?max_length=35')
   .then(r => r.json())
-  .then(d => {
+  .then(data => {
     const element = document.querySelector('.tagline');
-    element && (element.innerText = `『${d.hitokoto}』—— ${d.creator} 《${d.from}》`);
+    element && (element.innerText = `『${data.hitokoto}』—— ${data.creator} 《${data.from}》`);
+    console.log(data)
   })
   .catch(e => console.error('请求失败：', e));
